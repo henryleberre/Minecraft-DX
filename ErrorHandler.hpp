6c0d8426bb5e9035bb43d3d872a82a5861970285
PolarToCartesian <24208000+PolarToCartesian@users.noreply.github.com>
2020-11-24 23:08:17 +0100

    Fixed Build Errors & Build System

--- ErrorHandler.hpp
+++
@@ -1,7 +0,0 @@
-#include "Pch.hpp"
-
-#ifdef _WIN32
-
-    #define FATAL_ERROR(errorMsg) { MessageBoxA(NULL, errorMsg, "Minecraft: Fatal Error", MB_ICONERROR); std::exit(-1); }
-
-#endif // _WIN32
--- scripts/compile.bat
+++
@@ -1,4 +0,0 @@
-cl.exe Minecraft.cpp /fp:fast /arch:AVX /Fe:Minecraft-MD.exe /EHsc /MD /O2 /std:c++17 /I "C:\Code\tools\vcpkg\installed\x86-windows\include" /link /LIBPATH:"C:\Code\tools\vcpkg\installed\x86-windows\lib"
-cl.exe Minecraft.cpp /fp:fast /arch:AVX /Fe:Minecraft-MT.exe /EHsc /MT /O2 /std:c++17 /I "C:\Code\tools\vcpkg\installed\x86-windows\include" /link /LIBPATH:"C:\Code\tools\vcpkg\installed\x86-windows\lib"
-
-rm Minecraft.obj

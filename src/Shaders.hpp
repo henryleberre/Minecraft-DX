e3c45a52ea0553b80269eaf496d16bbc1bdb65a4
PolarToCartesian <24208000+PolarToCartesian@users.noreply.github.com>
2021-02-20 15:39:37 +0100

    Exp Fog

--- src/Shaders.hpp
+++ src/Shaders.hpp
@@ -10,6 +10,7 @@ inline const char* vsBlockCode = R"V0G0N(
         float4 position  : SV_POSITION;
         float2 uv        : UV;
         float  lighting  : LIGHTING;
+        float  fog       : FOG;
     };
 
     VS_OUTPUT main(float4 position : POSITION, float2 uv : UV, float lighting : LIGHTING) {
@@ -17,6 +18,8 @@ inline const char* vsBlockCode = R"V0G0N(
         result.position = mul(transform, position);
         result.uv       = uv;
         result.lighting = lighting;
+
+        result.fog      = 1.0 - 1.0/pow(2.71, 0.0025 * distance(position, float4(0.f, position.y, 0.f, 0.f)));
 
         return result;
     }
@@ -26,8 +29,8 @@ inline const char* psBlockCode = R"V0G0N(
     Texture2D    textureAtlas : register(t0);
     SamplerState samplerState : register(s0);
 
-    float4 main(float4 position : POSITION, float2 uv : UV, float lighting : LIGHTING) : SV_TARGET {
-        return float4(textureAtlas.Sample(samplerState, uv).xyz * lighting, 1.0f);
+    float4 main(float4 position : POSITION, float2 uv : UV, float lighting : LIGHTING, float fog : FOG) : SV_TARGET {
+        return float4(textureAtlas.Sample(samplerState, uv).xyz * lighting * (1 - fog) + fog * float3(1.f, 1.f, 1.f), 1.0f);
     }
 )V0G0N";
 
